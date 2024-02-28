f036cf7f1cfe8a3e76b01d5bebd9abc8cd9cbe68
MattOxEnjoyer <157534915+MattOxEnjoyer@users.noreply.github.com>
2024-02-28 19:42:30 +0500

    Add title n bg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Title from './Components/Title/Title';
 import Button from './Components/Button/Button';
 function App() {
   return (
     <div className="App">
+      <Title title="Andrew=Pidorsd" color="#292929"/>
       <Button title="Hello" color="#020202"/>
     </div>
   );
---
+++ frontend/src/Components/Title/Title.js
@@ -0,0 +1,10 @@
+import './Title.sass';
+function Tittle()
+{
+    return(
+        <div className="title">
+            <h1>Papper</h1>
+        </div>
+    )
+}
+export default Tittle
