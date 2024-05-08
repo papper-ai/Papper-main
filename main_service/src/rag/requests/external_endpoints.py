2086a01dba6f5a6a902f5febc373e4c83eb9838c
Egorov E.A <emelya.kara@yandex.ru>
2024-05-08 23:05:27 +0500

    Fix rag config

--- main_service/src/rag/requests/external_endpoints.py
+++ main_service/src/rag/requests/external_endpoints.py
@@ -5,7 +5,7 @@ from ..config import settings
 class RagEndpoints(BaseModel):
     graph_answer: str = f"{settings.graph_rag_service.graph_rag_service_url}/answer"
     vector_answer: str = (
-        f"{settings.graph_rag_service.vector_rag_service_url}/qa/answer"
+        f"{settings.vector_rag_service.vector_rag_service_url}/qa/answer"
     )
 
 
