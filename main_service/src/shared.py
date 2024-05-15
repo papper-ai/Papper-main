7670686d2251286682fb1ac90f17b85a83c7a3cc
Emelyan Egorov <113930312+Pomelkin@users.noreply.github.com>
2024-05-15 23:45:50 +0500

    Update shared.py

--- main_service/src/shared.py
+++ main_service/src/shared.py
@@ -1,4 +1,4 @@
 from transformers import AutoTokenizer
 
 ml_models = dict()
-ml_models["tokenizer"] = ml_models["tokenizer"] = AutoTokenizer.from_pretrained("lightblue/suzume-llama-3-8B-multilingual")
+ml_models["tokenizer"] = AutoTokenizer.from_pretrained("lightblue/suzume-llama-3-8B-multilingual")
